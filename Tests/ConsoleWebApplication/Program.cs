1a3812b2ec5260d62f13f707ec8194c5b80ac68f
Kerem AKTAŞ <kerem.aktas@pusulayazilim.com.tr>
2025-04-25 13:00:21 +0300

    Webapplication

--- Tests/ConsoleWebApplication/Program.cs
+++ Tests/ConsoleWebApplication/Program.cs
@@ -1,21 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using ConsoleWebApplication;
-using IQ.Mofy.Configuration.Extensions;
-using IQ.Mofy.Regify.Extensions;
+using IQ.Mofy.Core.App;
 using IQ.Mofy.Web.Api.App;
 using Microsoft.AspNetCore.Builder;
 
 var application = new WebApiApplication();
 
-application.UseRegify();
+application.ServiceCollection.AddRegify();
 
 await application.BuildHostAsync();
 
 application.MapGet("/", async (ICarRepository carRepository) =>
 {
     var query = await carRepository.GetQueryableAsync();
-    
+
     var cars = await carRepository.GetListAsync(d => true);
     return cars;
 });
