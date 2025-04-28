0bfdda715f157eb563cf85fe0ce07dbc1b5a5250
Kerem AKTAŞ <kerem.aktas@pusulayazilim.com.tr>
2025-04-28 19:12:07 +0300

    Ref

--- Tests/ConsoleWebApplication/ICarRepository.cs
+++ Tests/ConsoleWebApplication/ICarRepository.cs
@@ -4,4 +4,4 @@ namespace ConsoleWebApplication;
 
 public interface ICarRepository : IRepository<Car>;
 
-public interface IMercedesRepository : IRepository<Car>;
+public interface IMercedesCarRepository : IRepository<Car>;
